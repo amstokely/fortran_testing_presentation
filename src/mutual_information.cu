036a90a486d0406e50c8c4298e35ce43f2de107a
Andy Stokely <astokely@ucar.edu>
2026-02-05 14:49:00 -0700

    Updated cuda code.

--- src/mutual_information.cu
+++ src/mutual_information.cu
@@ -25,14 +25,14 @@ template<typename T> struct CudaArray {
 };
 
 struct CudaConfig {
-    int blockSize{};
-    int numBlocks{};
-    int registerSize{};
+    int block_dim_x{};
+    int grid_dim_x{};
+    int local_m_size{};
 
     explicit CudaConfig(const int n_points) {
-        blockSize = 1024;
-        numBlocks = (n_points + blockSize - 1) / blockSize;
-        registerSize = (n_points / blockSize) + 1;
+        block_dim_x = 1024;
+        grid_dim_x = (n_points + block_dim_x - 1) / block_dim_x;
+        local_m_size = (n_points / block_dim_x) + 1;
     }
 };
 
@@ -77,8 +77,7 @@ __device__ void min_warp_reduce_kernel(
     }
 }
 
-
-template<int XRegisterSize> __global__ void ksg_counts_kernel(
+template<int LocalMSize> __global__ void ksg_counts_kernel(
     const double *mx, const double *my, const int k, const int n_points,
     int *mx_counts, int *my_counts
 ) {
@@ -89,10 +88,10 @@ template<int XRegisterSize> __global__ void ksg_counts_kernel(
     __shared__ int s_mx_counts[1024];
     __shared__ int s_my_counts[1024];
     for (auto i = blockIdx.x; i < n_points; i += gridDim.x) {
-        double r_mx[XRegisterSize];
-        double r_my[XRegisterSize];
-        double r_mx_i = mx[i];
-        double r_my_i = my[i];
+        double l_mx[LocalMSize];
+        double l_my[LocalMSize];
+        double l_mx_i = mx[i];
+        double l_my_i = my[i];
         const unsigned int l_tidx = threadIdx.x;
         unsigned int idx = l_tidx;
         s_mx_counts[l_tidx] = 0;
@@ -101,28 +100,28 @@ template<int XRegisterSize> __global__ void ksg_counts_kernel(
             s_eps_mx[l_tidx] = 0.0;
             s_eps_my[l_tidx] = 0.0;
         }
-        int r_idx = 0;
+        int l_m_idx = 0;
         while (idx < n_points) {
-            r_mx[r_idx] = mx[idx];
-            r_my[r_idx] = my[idx];
+            l_mx[l_m_idx] = mx[idx];
+            l_my[l_m_idx] = my[idx];
             idx += blockDim.x;
-            r_idx++;
+            l_m_idx++;
         }
         __syncthreads();
         for (int j = 0; j < k + 1; j++) {
             idx = l_tidx;
             auto l_min = static_cast<double>(RAND_MAX);
             int l_min_idx = 0;
-            r_idx = 0;
+            l_m_idx = 0;
             while (idx < n_points) {
                 const double dX = fmax(
-                    abs(r_mx_i - r_mx[r_idx]), abs(r_my_i - r_my[r_idx])
+                    abs(l_mx_i - l_mx[l_m_idx]), abs(l_my_i - l_my[l_m_idx])
                 );
                 if (dX < l_min) {
                     l_min = dX;
                     l_min_idx = idx;
                 }
-                r_idx++;
+                l_m_idx++;
                 idx += blockDim.x;
             }
             s_min[l_tidx] = l_min;
@@ -166,30 +165,30 @@ template<int XRegisterSize> __global__ void ksg_counts_kernel(
                 if (j > 0) {
                     s_eps_mx[0] = fmax(
                         s_eps_mx[0], abs(
-                            r_mx_i - r_mx[s_min_idx[0] / blockDim.x]
+                            l_mx_i - l_mx[s_min_idx[0] / blockDim.x]
                         )
                     );
                     s_eps_my[0] = fmax(
                         s_eps_my[0], abs(
-                            r_my_i - r_my[s_min_idx[0] / blockDim.x]
+                            l_my_i - l_my[s_min_idx[0] / blockDim.x]
                         )
                     );
                 }
-                r_mx[s_min_idx[0] / blockDim.x] = static_cast<double>(
+                l_mx[s_min_idx[0] / blockDim.x] = static_cast<double>(
                     RAND_MAX);
-                r_my[s_min_idx[0] / blockDim.x] = static_cast<double>(
+                l_my[s_min_idx[0] / blockDim.x] = static_cast<double>(
                     RAND_MAX);
             }
             __syncthreads();
         }
         idx = l_tidx;
-        r_idx = 0;
+        l_m_idx = 0;
         while (idx < n_points) {
             s_mx_counts[l_tidx] +=
-                    (abs(r_mx[r_idx] - r_mx_i) <= s_eps_mx[0]) ? 1 : 0;
+                    (abs(l_mx[l_m_idx] - l_mx_i) <= s_eps_mx[0]) ? 1 : 0;
             s_my_counts[l_tidx] +=
-                    (abs(r_my[r_idx] - r_my_i) <= s_eps_my[0]) ? 1 : 0;
-            r_idx++;
+                    (abs(l_my[l_m_idx] - l_my_i) <= s_eps_my[0]) ? 1 : 0;
+            l_m_idx++;
             idx += blockDim.x;
         }
         __syncthreads();
@@ -227,24 +226,25 @@ template<int XRegisterSize> __global__ void ksg_counts_kernel(
 }
 
 
-template<int R> void launch_kernel(
-    const double *d_Mx, const double *d_My, int k, const int n_points,
-    int *d_nx, int *d_ny, int numBlocks, int blockSize
+template<int LocalMSize> void launch_kernel(
+    const double *d_mx, const double *d_my, int k, const int n_points,
+    int *d_mx_counts, int *d_my_counts, int grid_dim_x, int block_dim_x
 ) {
-    ksg_counts_kernel<R> <<<numBlocks, blockSize>>>(
-        d_Mx, d_My, k, n_points, d_nx, d_ny
+    ksg_counts_kernel<LocalMSize> <<<grid_dim_x, block_dim_x>>>(
+        d_mx, d_my, k, n_points, d_mx_counts, d_my_counts
     );
 }
 
 template<int... I> void dispatch_kernel(
-    int r, std::integer_sequence<int, I...>, const double *d_Mx,
-    const double *d_My, int k, int n_points, int *d_nx, int *d_ny,
-    const int numBlocks, const int blockSize
+    int local_m_size, std::integer_sequence<int, I...>,
+    const double *d_mx, const double *d_my, const int k,
+    const int n_points, int *d_mx_counts, int *d_my_counts,
+    const int grid_dim_x, const int block_dim_x
 ) {
-    ((r == I + 1 ? (launch_kernel<I + 1>(
-                        d_Mx, d_My, k, n_points, d_nx, d_ny, numBlocks,
-                        blockSize
-                    ), 0) : 0), ...);
+    ((local_m_size == I + 1 ? (launch_kernel<I + 1>(
+                                   d_mx, d_my, k, n_points, d_mx_counts,
+                                   d_my_counts, grid_dim_x, block_dim_x
+                               ), 0) : 0), ...);
 }
 
 namespace ksg {
@@ -264,10 +264,10 @@ namespace ksg {
         const auto config = CudaConfig(n_points);
 
         dispatch_kernel(
-            config.registerSize, std::make_integer_sequence<int, 101>{},
+            config.local_m_size, std::make_integer_sequence<int, 101>{},
             cuda_mx.d_array, cuda_my.d_array, k, n_points,
-            cuda_nx.d_array, cuda_ny.d_array, config.numBlocks,
-            config.blockSize
+            cuda_nx.d_array, cuda_ny.d_array, config.grid_dim_x,
+            config.block_dim_x
         );
 
         cuda_nx.to_host(mx_counts);
