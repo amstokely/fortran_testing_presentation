61276e615c058a84446b8ebd80a0c521c8e2cf19
Andy Stokely <astokely@ucar.edu>
2026-02-09 12:35:43 -0700

    Fixed non-std compliant use of std::tie.

--- src/mutual_information.cuh
+++ src/mutual_information.cuh
@@ -1,5 +1,6 @@
 #ifndef FORTRAN_TESTING_PRESENTATION_MUTUAL_INFORMATION_CUH
 #define FORTRAN_TESTING_PRESENTATION_MUTUAL_INFORMATION_CUH
+#include <span>
 
 struct cuda_ksg_counts {
     void operator()(std::span<const double> Mx, std::span<const double> My,
